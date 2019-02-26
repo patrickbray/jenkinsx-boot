e15cb400e5466eca7a0ba7115a04ce2fd45b1994
Patrick Bray <pbray@atlassian.com>
2019-02-26 19:44:51 +1300

    Add Quote API.

--- src/main/java/com/pat/solutions/jenkinsxboot/DemoApplication.java
+++ src/main/java/com/pat/solutions/jenkinsxboot/DemoApplication.java
@@ -2,6 +2,9 @@ package com.pat.solutions.jenkinsxboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +13,8 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Bean
+	public RestTemplate createRestTemplate(RestTemplateBuilder restTemplateBuilder) {
+	    return restTemplateBuilder.build();
+    }
 }
